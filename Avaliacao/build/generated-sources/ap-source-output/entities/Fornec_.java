3658f41566c5e4aad4fca92ff5a76367a219280b
gonra <gonzalito9221@hotmail.com>
2016-07-11 12:32:05 -0300

    Upload fontes

---
+++ Avaliacao/build/generated-sources/ap-source-output/entities/Fornec_.java
@@ -0,0 +1,17 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-10T15:31:11")
+@StaticMetamodel(Fornec.class)
+public class Fornec_ { 
+
+    public static volatile SingularAttribute<Fornec, String> name;
+    public static volatile SingularAttribute<Fornec, String> comment;
+    public static volatile SingularAttribute<Fornec, Integer> id;
+    public static volatile SingularAttribute<Fornec, String> cnpj;
+    public static volatile SingularAttribute<Fornec, String> email;
+
+}
---
+++ Avaliacao/src/java/entities/Fornec.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author gonra
+ */
+@Entity
+@Table(name = "fornec")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Fornec.findAll", query = "SELECT f FROM Fornec f"),
+    @NamedQuery(name = "Fornec.findById", query = "SELECT f FROM Fornec f WHERE f.id = :id"),
+    @NamedQuery(name = "Fornec.findByName", query = "SELECT f FROM Fornec f WHERE f.name = :name"),
+    @NamedQuery(name = "Fornec.findByEmail", query = "SELECT f FROM Fornec f WHERE f.email = :email"),
+    @NamedQuery(name = "Fornec.findByComment", query = "SELECT f FROM Fornec f WHERE f.comment = :comment"),
+    @NamedQuery(name = "Fornec.findByCnpj", query = "SELECT f FROM Fornec f WHERE f.cnpj = :cnpj")})
+public class Fornec implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 45)
+    @Column(name = "name")
+    private String name;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="E-mail inválido")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 45)
+    @Column(name = "email")
+    private String email;
+    @Size(max = 45)
+    @Column(name = "comment")
+    private String comment;
+    @Size(max = 45)
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    public Fornec() {
+    }
+
+    public Fornec(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Fornec)) {
+            return false;
+        }
+        Fornec other = (Fornec) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Fornec[ id=" + id + " ]";
+    }
+    
+}
